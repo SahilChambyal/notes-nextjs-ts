c5a5bdfe064bb766be6b8f7e22399db3954d7171
Sahil Chambyal <chambyal20062003@gmail.com>
2024-07-16 12:54:38 +0530

    progressBar repair

--- components/progressBar.tsx
+++ components/progressBar.tsx
@@ -1,22 +1,26 @@
-'use client'
+'use client';
 
-const ProgressBar = ({ progress , color}:{progress:any, color:string}) => {
+import React from 'react';
 
-    const styleObject = {
-        width: `${progress}%`,
-        backgroundColor: color || '#333',
-        height: 5,
-        borderRadius: 20
-    }
-    return (
-        <div className="mt-[50px] w-[300px] sm:w-[400px] md:w-[600px] ">
-            <div className="h-[5px] w-[100%] rounded-[20px] text-center ">
-                <div style={styleObject}>
-                    
-                </div>
-            </div>
-        </div>
-    )
+interface ProgressBarProps {
+  progress: number;
+  color: string;
 }
+
+const ProgressBar: React.FC<ProgressBarProps> = ({ progress, color }) => {
+  return (
+    <div className="mt-[50px] w-[300px] sm:w-[400px] md:w-[600px]">
+      <div className="h-[5px] w-full rounded-[20px] bg-gray-200">
+        <div
+          className="h-full rounded-[20px]"
+          style={{
+            width: `${progress}%`,
+            backgroundColor: color || '#333',
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+};
 
 export default ProgressBar;
