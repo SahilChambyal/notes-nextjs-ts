97eabb6f00327c5b607b933756d51ea473f847e7
Sahil Chambyal <chambyal20062003@gmail.com>
2024-07-16 13:29:47 +0530

    repairingLoading

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,39 +2,37 @@
 
 import React, { useEffect, useState } from 'react';
 import ProgressBar from '@/components/progressBar';
-import {  PenLine } from 'lucide-react';
+import { PenLine } from 'lucide-react';
 
 const Loading = () => {
   const [progress, setProgress] = useState(0);
 
   useEffect(() => {
-    const time = setInterval(() => {
-      if (progress < 100) {
-        setProgress((p) => p + 1);
-        console.log(progress);
-      }
+    const interval = setInterval(() => {
+      setProgress((p) => {
+        if (p < 100) {
+          return p + 1;
+        } else {
+          clearInterval(interval);
+          return p;
+        }
+      });
+      console.log(progress)
     }, 20);
 
     return () => {
-      clearInterval(time);
-    }
-  }, [progress])
-
-  // useEffect(() => {
-  //   const timeout = setTimeout(() => console.log('loading'), 2000);
-  //   // Cleanup timeout on component unmount
-  //   return () => clearTimeout(timeout);
-  // }, []);
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <div className='flex flex-col items-center justify-center mt-[300px]'>
-      <div className='text-2xl font-bold text-[#333] flex '>
+      <div className='text-2xl font-bold text-[#333] flex'>
         <PenLine />
         Get Notes
       </div>
       <div>
-
-      <ProgressBar progress={progress} color='#333' />
+        <ProgressBar progress={progress} color='#333' />
       </div>
     </div>
   );
